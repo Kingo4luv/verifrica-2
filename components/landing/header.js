c8a05126f54452074a7e1ed421269e4bf9ce097e
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-07-01 22:11:26 +0100

    hero space

--- components/landing/header.js
+++ components/landing/header.js
@@ -24,7 +24,7 @@ const HeaderComponent = () => {
                 <h1 className="text-left md:text-center text-[23px] leading-[32.68px] md:text-4xl lg:text-headline-3 font-bold text-white">
                   Connect financial accounts to apps you love in seconds.
                 </h1>
-                <p className="mt-6 md:mt-12 text-white font-light text-left md:text-center max-w-xl mx-auto text-sm md:text-base">Verifrica provides a seamless and convenient way of connecting people's financial accounts to an app with easy and secure access</p>
+                <p className="mt-6 text-white font-light text-left md:text-center max-w-xl mx-auto text-sm md:text-base">Verifrica provides a seamless and convenient way of connecting people's financial accounts to an app with easy and secure access</p>
                 <div className="mt-12 flex flex-col md:flex-row space-x-0 md:space-x-5 space-y-4 md:space-y-0 items-center justify-center">
                    <div className="w-full md:w-52">
                      <LinkButton color="yellow" url="/" text="Get Started" />
